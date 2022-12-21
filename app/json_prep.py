2c3e4568dc5b0dbc331057551dddf1d19ab8d165
loughness <21imc10058@fh-krems.ac.at>
2022-12-21 13:46:53 +0100

    JSON Prep working

--- app/json_prep.py
+++ app/json_prep.py
@@ -53,9 +53,6 @@ class json_obj:
         self.data = json.loads(workout_json_template)
         self.workout_data = self.data["workout"]
 
-        self.ex_data = json.loads(exercise_json_template)
-
-
     def set_date(self, date):
         self.workout_data["date"] = date
 
@@ -74,34 +71,73 @@ class json_obj:
     def get_start_note(self):
         return self.workout_data["start_note"]
 
-    def set_ex_name(self, ex_name):
-        self.ex_data["exercise_name"] = ex_name
-        return self.ex_data
+    def set_end_note(self, end_note):
+        self.workout_data["end_note"] = end_note
 
-    def set_initial_sets(self, sets):
-        self.ex_data["initial_sets"] = sets
-        return self.ex_data
+    def set_workout_score(self, score):
+        self.workout_data["workout_rating"] = score
 
-    def create_exercise(self, name):
+    def set_ex_name(self, exercise_temp, ex_name):
+        exercise_temp["exercise_name"] = ex_name
+        return exercise_temp
 
-        self.workout_data["exercises"].append(self.set_ex_name(name))
-        self.workout_data["exercises"].append(self.set_initial_sets(12))
+    def set_initial_sets(self, exercise_temp, sets):
+        exercise_temp["initial_sets"] = sets
+        return exercise_temp
+
+    def set_initial_reps(self, exercise_temp, reps):
+        exercise_temp["initial_reps"] = reps
+        return exercise_temp
+
+    def create_exercise(self, name, initial_sets, initial_reps):
+        new_ex_file = json.loads(exercise_json_template)
+        # created a new exercise file here so that it will not reference one that was made once
+        # at the beginning of the class
+        exercise = self.set_ex_name(new_ex_file, name)
+        # self.workout_data["exercises"].append(ex_name)
+        exercise = self.set_initial_sets(new_ex_file, initial_sets)
+        exercise = self.set_initial_reps(new_ex_file, initial_reps)
+
+        self.workout_data["exercises"].append(exercise)
+
+    def add_set(self, weight, reps, difficulty, tempo = None):
+        set = [weight, reps, tempo, difficulty]
+        exercise = self.workout_data["exercises"]
+        exercise[-1]["sets"].append(set)
+
+    def add_exercise_note(self, ex_note):
+        exercise = self.workout_data["exercises"]
+        exercise[-1]["exercise_note"]  = ex_note
+
 
     def overview(self):
-        print(self.workout_data["exercises"])
-        # print(self.ex_data)
+        print("OVERVIEW")
+        print("WORKOUT_DATA")
+        print(self.workout_data)
+        print("***")
 
 
 
 new_file = json_obj()
 new_file.set_date(23)
 new_file.set_title("December the fifteenth")
+new_file.set_start_note("Working all over today")
 new_file.overview()
-new_file.create_exercise("Curls")
+new_file.create_exercise("Curls", 3, 12)
+new_file.add_set(14,12,1,"paced")
+new_file.add_set(16,12,1,"paced")
+new_file.add_set(20,8,3,"slow")
 new_file.overview()
-new_file.create_exercise("Bench")
+new_file.create_exercise("Bench", 4, 10)
+new_file.add_set(60,12,2,"paced")
+new_file.overview()
+new_file.create_exercise("Pull-ups", 5, 10)
+new_file.add_set(0,12,1,"paced")
+new_file.add_exercise_note("Tough on the back")
+new_file.set_end_note("Fun day pushing hard!")
+new_file.set_workout_score(2)
+new_file.overview()
 
-new_file.overview()
 
 
 
